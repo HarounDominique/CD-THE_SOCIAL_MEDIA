7bf92ac2a90efba0dccf2967146e095eead3ce0f
HAROUN <haroundominique@gmail.com>
2023-09-05 12:01:59 +0200

    finished signin method

--- src/main/java/com/campusdual/TheSocialMedia.java
+++ src/main/java/com/campusdual/TheSocialMedia.java
@@ -71,7 +71,7 @@ public class TheSocialMedia {
     /**
      * CUSTOM METHODS
      **/
-    public boolean addUser() {
+    public boolean login() {
         String userMail;
         String userName;
         String userPass;
@@ -186,6 +186,65 @@ public class TheSocialMedia {
         return validPass;
     }
 
+    public void signin(){
+
+        boolean control = false;
+
+        boolean matchingUser = false;
+        String userMail;
+        String userPass;
+        do {
+            System.out.println("*********************     SIGNIN     *********************");
+            System.out.println("*                                                        *");
+            System.out.println("*                    Insert your EMAIL                   *");
+            System.out.println("*                                                        *");
+            System.out.print("* >>> ");
+            userMail = Input.string();
+            if (userMail.trim().isEmpty()) {
+                System.out.println("INVALID EMAIL: NO EMPTY FIELDS");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println("*********************     SIGNIN     *********************");
+                System.out.println("*                                                        *");
+                System.out.println("*                  Insert your PASSWORD                  *");
+                System.out.println("*                                                        *");
+                System.out.print("* >>> ");
+                userPass = Input.string();
+                if (userPass.trim().isEmpty()) {
+                    System.out.println("INVALID PASSWORD: NO EMPTY FIELDS");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else {
+                    int mailIndex = mailList.indexOf(userMail);
+                    int passIndex = passList.indexOf(userPass);
+                    if(mailIndex == passIndex){
+                        matchingUser = true;
+                        loggedUi();
+                    }else{
+                        System.out.println("THERE IS NO USER THAT MATCHES THOSE DATA");
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        control = true;
+                        ui();
+                    }
+                }
+            }
+            if(control){
+                System.exit(0);
+            }
+        } while (!matchingUser);
+    }
+
     public boolean removeUser(User u) {
         boolean userRemoved = false;
         Iterator<User> iterator = this.userList.iterator();
@@ -239,13 +298,51 @@ public class TheSocialMedia {
             System.out.println("*              Insert '3' to SHOW USER LIST              *");
             System.out.print("* >>> ");
             String answer = Input.string();
-            if (answer.trim().equals("0")) {
-                System.out.println("BYE");
-                on = false;
-            }else if(answer.trim().equals("1")){
-                addUser();
-            }else if(answer.trim().equals("3")){
-                System.out.println(toString());
+            switch (answer.trim()){
+                case "0":
+                    on = false;
+                    System.exit(0);
+                    break;
+                case "1":
+                    login();
+                    break;
+                case "2":
+                    signin();
+                    break;
+                case "3":
+                    System.out.println(toString());
+            }
+        } while (on);
+    }
+
+    public void loggedUi(){
+        boolean on = true;
+        do {
+            System.out.println("*********************THE SOCIAL MEDIA*********************");
+            System.out.println("**********************************************************");
+            System.out.println("**********************YOU ARE LOGGED**********************");
+            System.out.println("*                   Insert '0' to EXIT                   *");
+            System.out.println("*                                                        *");
+            System.out.println("*                   Insert '1' to LOGIN                  *");
+            System.out.println("*                                                        *");
+            System.out.println("*                  Insert '2' to SIGN IN                 *");
+            System.out.println("*                                                        *");
+            System.out.println("*              Insert '3' to SHOW USER LIST              *");
+            System.out.print("* >>> ");
+            String answer = Input.string();
+            switch (answer.trim()){
+                case "0":
+                    on = false;
+                    System.exit(0);
+                    break;
+                case "1":
+                    login();
+                    break;
+                case "2":
+                    signin();
+                    break;
+                case "3":
+                    System.out.println(toString());
             }
         } while (on);
     }
