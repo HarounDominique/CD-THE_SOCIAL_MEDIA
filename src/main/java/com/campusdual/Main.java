6ee7bca79bebe6cc9a5a3f51f4c5ee33e35b2775
HAROUN <haroundominique@gmail.com>
2023-09-06 14:04:00 +0200

    fin red social -incompleto-

--- src/main/java/com/campusdual/Main.java
+++ src/main/java/com/campusdual/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         TheSocialMedia tsm = new TheSocialMedia();
         ArrayList<User> ua = new ArrayList<>();
+        /*
         User u1 = new User("user1", "mail1", "pass1");
 
         User u2 = new User("user2", "mail2", "pass2");
@@ -22,6 +23,7 @@ public class Main {
         ua.add(u3);
         ua.add(u4);
         ua.add(u5);
+         */
         tsm.setUserList(ua);
         tsm.ui();
     }
